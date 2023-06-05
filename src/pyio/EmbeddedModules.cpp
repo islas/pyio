d68de2bd6ac74ed64d60b4ed0da8f2899528c692
Anthony Islas <aislas@ucar.edu>
2023-06-05 13:46:32 -0600

    Declarations for embedded python modules

---
+++ src/pyio/EmbeddedModules.cpp
@@ -0,0 +1,28 @@
+#include "pybind11/pybind11.hpp"
+
+PYBIND11_EMBEDDED_MODULE( static_data, m )
+{
+  // Place holder for adding to later
+}
+
+PYBIND11_EMBEDDED_MODULE( runtime_data, m )
+{
+  // Place holder for adding to later
+}
+
+PYBIND11_EMBEDDED_MODULE( helper, m )
+{
+  // Place holder for adding to later
+}
+
+PYBIND11_EMBEDDED_MODULE( demo, m )
+{
+  // Place holder for adding to later
+}
+
+#ifdef SOME_FEATURE
+PYBIND11_EMBEDDED_MODULE( feature_mod, m )
+{
+  // Place holder for adding to later
+}
+#endif
