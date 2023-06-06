08a0f7c9195d8826b6f98dd70022de8af4bf0d02
Anthony Islas <aislas@ucar.edu>
2023-06-06 11:54:20 -0600

    Switching to subroutines

--- src/main/driver.f90
+++ src/main/driver.f90
@@ -2,32 +2,38 @@
 program driver
   use iso_c_binding
   use EmbeddedInterpreter
-  use f_c_string
+  use f_c_helpers
 
   implicit none
 
-  type( c_ptr )         :: interpreter
+  type( c_ptr )         :: interpreter = c_null_ptr
   real, dimension( 10 ) :: arr = [ 1:10 ]
+  integer( c_size_t ), dimension(1) :: dims = [ 10 ]
+  integer( c_size_t )                :: numDims = 1
 
-  interpreter = EmbeddedInterpreter_ctor()
+  call EmbeddedInterpreter_ctor( interpreter )
+  
 
-  EmbeddedInterpreter_initialize( interpreter )
-  EmbeddedInterpreter_addToScope( interpreter,  f_c_string( "/glade/u/home/aislas/pythonBindings/demo/pymodules/" ) )
+  call EmbeddedInterpreter_initialize( interpreter )
+  call EmbeddedInterpreter_addToScope( interpreter,  f_c_string( "/glade/u/home/aislas/pythonBindings/demo/pymodules/" ) )
   
   
   ! Add embedded values
-  EmbeddedInterpreter_embedFloatPtr( interpreter, f_c_string( "static_data" ), f_c_string( "arr" ), arr, 1, [ 10 ] )
+  call EmbeddedInterpreter_embedFloatPtr( interpreter, f_c_string( "static_data" ), f_c_string( "arr" ), arr, numDims, dims )
   
   ! Use user module
-  EmbeddedInterpreter_pymoduleLoad( interpreter,  f_c_string( "interp.euler" ) )
-  EmbeddedInterpreter_pymoduleInitialize( interpreter,  f_c_string( "interp.euler" ) )
+  call EmbeddedInterpreter_pymoduleLoad( interpreter,  f_c_string( "interp.euler" ) )
+  call EmbeddedInterpreter_pymoduleInitialize( interpreter,  f_c_string( "interp.euler" ) )
 
-  EmbeddedInterpreter_pymoduleCall( interpreter,  f_c_string( "interp.euler" ) )
+  call EmbeddedInterpreter_pymoduleCall( interpreter,  f_c_string( "interp.euler" ) )
 
 
-  EmbeddedInterpreter_pymoduleFinalize( interpreter,  f_c_string( "interp.euler" ) )
+  call EmbeddedInterpreter_pymoduleFinalize( interpreter,  f_c_string( "interp.euler" ) )
 
   ! finalize
-  EmbeddedInterpreter_finalize( interpreter )
+  call EmbeddedInterpreter_finalize( interpreter )
+
+  ! delete
+  call EmbeddedInterpreter_dtor( interpreter )
   
 end program driver
